ace6b96ecec76685064ddabe2ddef7ac33acfc70
Roshni Gannoju <71202171+gannoju15roshni@users.noreply.github.com>
2025-01-09 23:34:32 -0500

    sets.py updated

--- Day 1/sets.py
+++ Day 1/sets.py
@@ -1,6 +1,16 @@
 set1={"hp",5,"dell",1}
 set1.add("lenovo")
 set2={3,"mac",4}
-set1.update(set2)
-set1.discard("mac")
-print(set1)
+set3={"msi",1,"alien ware",6}
+set4={"legion",2,"asus",7}
+set5=set3 | set4 | set2 | set1
+print(set5)
+
+seta={"apple","banana","cherry"}
+setb={"banana","apple"}
+
+setc=seta.symmetric_difference(setb)
+print(setc)
+# set1.update(set2)
+# set1.discard("mac")
+# print(set1)
