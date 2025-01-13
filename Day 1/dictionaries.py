be680d5b3640daf74ff2a6deb282a218c28b6c05
Roshni Gannoju <71202171+gannoju15roshni@users.noreply.github.com>
2025-01-12 23:51:27 -0500

    Updated dictionaries.py

--- Day 1/dictionaries.py
+++ Day 1/dictionaries.py
@@ -4,7 +4,40 @@ dict1={
     "yellow":"sunflower"
 }
 
+print(dict1.get("yellow"))
+
+#  adding a new item using key() method
+x=dict1.keys()
+
+print(x)
+
+dict1["pink"]="lotus"
+
+print(x)
+
+#  replacing the exsiting item using values() method
+y=dict1.values()
+
+print(y)
+
+dict1["white"]="tulip"
+
+print(y)
+
+# updating existing dictionary using update() method
+
+dict1.update({"white":"lily"})
+
 print(dict1)
 
+# you can remove an item using -> [dictionaryname.pop("key")]
+# -> [dictionaryname.popitem()] would remove the last inserted item
 
-#blank commit
+# you can use -> [del dictionaryname.["key"]] to delete specified key from the dictionary
+# remember if ->[ del dictinaryname ] is used and then printed you'll get an error since the dictionary is deleted before print command
+
+# you can clear the whole dictionary using -> [dictionaryname.clear()]
+
+# copy the exsisting dictionary assigning the -> [dictionaryname.copy()] to a variable and printing it
+# you can also copy using dict() method -> [dict(dictionaryname)]
+
