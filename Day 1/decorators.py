b93613f0b92eedbb94f6dcf49921d7839288f66d
Roshni Gannoju <71202171+gannoju15roshni@users.noreply.github.com>
2025-01-18 23:22:33 -0500

    Updated decorators.py

--- Day 1/decorators.py
+++ Day 1/decorators.py
@@ -1,22 +1,39 @@
 # Decorators are powerful and flexible way to modify or extend the behavior of functions or methods, without changing their actual code.
 
-# A decorator is essentially a fucntion that taskes another function as an argumnetb and returns a new function with enhanced fucntionality.
+# A decorator is essentially a fucntion that taskes another function as an argumnet and returns a new function with enhanced fucntionality.
 
 # Decorators are often used in scenariors such as logging, authentication and memorization, allowing us to and additional fucntionality to existing functions or methods in a clean, reusable way.
 
 # Simple Decorator Function:
-def decorator(func):
+
+# def decorator(func):
   
-    def wrapper():
-        print("Before calling the function.")
-        func()
-        print("After calling the function.")
-    return wrapper
+#     def wrapper():
+#         print("Before calling the function.")
+#         func()
+#         print("After calling the function.")
+#     return wrapper
 
-# Applying the decorator to a function
-@decorator
+# # Applying the decorator to a function
+# @decorator
 
-def greet():
-    print("Hello, World!")
+# def greet():
 
-greet()
+#     print("Hello, World!")
+
+# greet()
+
+
+# Example
+
+# A higher-order function that takes another function as an argument
+def fun(f, x):
+    return f(x)
+
+# A simple function to pass
+def square(x):
+    return x * x
+
+# Using apply_function to apply the square function
+res = fun(square, 5)
+print(res)
